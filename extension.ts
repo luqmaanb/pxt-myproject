363f77247a9d464ca3de2c124644e7ae99266192
luqmaanb <lb@xinabox.cc>
2020-03-06 15:13:03 +0200

    .

---
+++ extension.ts
@@ -0,0 +1,11 @@
+//% color=#444444 icon="\f018"
+namespace pxt_myProject {
+    /**
+     * Help goes here.
+     */
+    //% shim=pxt_myProject::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
